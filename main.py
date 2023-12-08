0442f7b760a135df7901d326fe4730f463695b71
Julio Raicher <julioraicher@gmail.com>
2023-12-08 05:33:55 -0300

    testando imagens
    aqui estou testando tamanhos das imagens blitando na tela

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import pygame
 import os
 import random
-
+from time import sleep
 
 # ir testando esses valores
-TELA_LARGURA = 800
-TELA_ALTURA = 500
+TELA_LARGURA = 1200
+TELA_ALTURA = 600
 
-IMAGEM_XWING = pygame.transform.scale(pygame.image.load(os.path.join('imgs', 'xwing.png')), (50, 50))
-IMAGEM_BACKGROUND = pygame.image.load(os.path.join('imgs', 'bg.jpg')))
-IMAGEM_METEORO = pygame.transform.scale(pygame.image.load(os.path.join('imgs', 'meteoro.png')), (25, 25))
-IMAGEM_LASER = pygame.transform.scale(pygame.image.load(os.path.join('imgs', 'laser.png')), (10, 10))
-
-
+IMAGEM_XWING = pygame.transform.scale(pygame.image.load(os.path.join('imgs', 'xwing.png')), (75, 70))
+IMAGEM_BACKGROUND = pygame.image.load(os.path.join('imgs', 'bg.jpg'))
+IMAGEM_METEORO = pygame.transform.scale(pygame.image.load(os.path.join('imgs', 'meteoro.png')), (60, 40))
+IMAGEM_LASER = pygame.transform.scale(pygame.image.load(os.path.join('imgs', 'laser.png')), (15, 15))
 
 
 # testando o tamanho das imagens desenhando na tela (desenhar())
 tela = pygame.display.set_mode((TELA_LARGURA, TELA_ALTURA))
+sleep(1)
 tela.blit(IMAGEM_BACKGROUND, (0, 0))
+tela.blit(IMAGEM_METEORO, (600, 300))
+tela.blit(IMAGEM_XWING, (600, 150))
+
+pygame.display.update()
+
 
 rodando = True
 while rodando:
